63442d35ebfd8dba754859b30bf5e1b6e5c6850f
Thomas Leese <inbox@thomasleese.me>
2016-09-07 16:08:28 +0100

    Initial Commit

---
+++ Average Speed Check/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Average Speed Check
+//
+//  Created by Thomas Leese on 07/09/2016.
+//  Copyright © 2016 Thomas Leese. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
