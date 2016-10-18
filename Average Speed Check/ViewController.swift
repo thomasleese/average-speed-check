852aa349c147e6b76193ad423ad247746db14b6b
Thomas Leese <inbox@thomasleese.me>
2016-10-18 21:47:20 +0100

    Update code

--- Average Speed Check/ViewController.swift
+++ Average Speed Check/ViewController.swift
@@ -6,20 +6,85 @@
 //  Copyright © 2016 Thomas Leese. All rights reserved.
 //
 
+import CoreLocation
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    var locationManager: CLLocationManager?
+
+    var timer: Timer?
+
+    var currentSpeed: Double = 0
+    var totalSpeed: Double = 0
+    var totalSpeedCount: Int = 0
+
+    @IBOutlet weak var label: UILabel!
+
+    func startLocationServices() {
+        locationManager = CLLocationManager()
+
+        locationManager?.delegate = self
+        locationManager?.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager?.distanceFilter = 2
+
+        locationManager?.requestWhenInUseAuthorization()
+
+        locationManager?.startUpdatingLocation()
+
+        print("Started recording locations.")
+    }
+
+    func stopLocationServices() {
+        locationManager?.stopUpdatingLocation()
+
+        print("Stopped recording locations.")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        startLocationServices()
+
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateLabel), userInfo: nil, repeats: true)
     }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        stopLocationServices()
+
+        timer?.invalidate()
+    }
+
+    func metersPerSecondToMilesPerHour(_ value: Double) -> Double {
+        return value * 2.23694
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last!
+
+        let speed = location.speed
+
+        if speed != -1 {
+            currentSpeed = speed
+        }
+
+        print("Got current speed as:", currentSpeed)
+    }
+
+    func updateLabel() {
+        totalSpeed += currentSpeed
+        totalSpeedCount += 1
+
+        let averageSpeedMps = totalSpeed / Double(totalSpeedCount)
+        let averageSpeed = metersPerSecondToMilesPerHour(averageSpeedMps)
+
+        label.text = String(format: "%.1f", averageSpeed) + " mph"
+    }
 
 }
-
